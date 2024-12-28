a6f3818f7dbc09e834e96204a0ee95dd4a0aea2f
Andy Cuong <nguyenduccuong11102003@gmail.com>
2024-12-28 18:26:02 +0200

    Create Material Theme

--- app/src/main/java/com/andyc/runique/MainActivity.kt
+++ app/src/main/java/com/andyc/runique/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.andyc.runique.ui.theme.RuniqueTheme
+import com.andyc.core.presentation.designsystem.AnalyticsIcon
+import com.andyc.core.presentation.designsystem.RuniqueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             RuniqueTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        Text(text = "Hello Android!")
+                        Icon(imageVector = AnalyticsIcon, contentDescription = null)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RuniqueTheme {
-        Greeting("Android")
     }
 }
