75cfda3c851395b5bdaced4e8c3047f20b2fd32b
Andy Cuong <nguyenduccuong11102003@gmail.com>
2025-01-02 01:39:44 +0200

    Create user data validation logic

--- auth/domain/src/main/java/com/andyc/auth/domain/UserDataValidator.kt
+++ auth/domain/src/main/java/com/andyc/auth/domain/UserDataValidator.kt
@@ -1,6 +1,30 @@
 package com.andyc.auth.domain
 
-class UserDataValidator {
+class UserDataValidator(
+    /**
+     * Pattern matcher is only available in Android library, which cannot be accessed by domain
+     * layer, so it is made abstract so that data layer can handle the real implementation
+     */
+    private val patternValidator: PatternValidator
+) {
+    fun isValidEmail(email: String): Boolean {
+        return patternValidator.matches(email.trim())
+    }
+
+    fun validatePassword(password: String): PasswordValidationState {
+        val hasMinLength = password.length >= MIN_PASSWORD_LENGTH
+        val hasDigit = password.any { it.isDigit() }
+        val hasLowerCaseCharacter = password.any { it.isLowerCase() }
+        val hasUpperCaseCharacter = password.any { it.isUpperCase() }
+
+        return PasswordValidationState(
+            hasMinLength = hasMinLength,
+            hasNumber = hasDigit,
+            hasLowerCaseCharacter = hasLowerCaseCharacter,
+            hasUpperCaseCharacter = hasUpperCaseCharacter
+        )
+    }
+
     companion object {
         const val MIN_PASSWORD_LENGTH = 9
     }
