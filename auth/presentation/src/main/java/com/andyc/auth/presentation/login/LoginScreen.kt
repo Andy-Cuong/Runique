c99e57c65d2b78eb6c64b4e90e9b4c4a574ca55b
Andy Cuong <nguyenduccuong11102003@gmail.com>
2025-01-06 05:24:55 +0200

    Create Login Screen UI

--- auth/presentation/src/main/java/com/andyc/auth/presentation/login/LoginScreen.kt
+++ auth/presentation/src/main/java/com/andyc/auth/presentation/login/LoginScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.withLink
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -113,7 +114,8 @@ fun LoginScreen(
                 endIcon = null,
                 hint = stringResource(id = R.string.example_email),
                 title = stringResource(id = R.string.email),
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                keyboardType = KeyboardType.Email
             )
             Spacer(modifier = Modifier.height(16.dp))
             RuniquePasswordTextField(
@@ -130,7 +132,7 @@ fun LoginScreen(
             RuniqueActionButton(
                 text = stringResource(id = R.string.log_in),
                 isLoading = state.isLoggingIn,
-                enabled = state.canLogin,
+                enabled = state.canLogin && !state.isLoggingIn,
             ) {
                 onAction(LoginAction.OnLoginClick)
             }
--- auth/presentation/src/main/java/com/andyc/auth/presentation/login/LoginViewModel.kt
+++ auth/presentation/src/main/java/com/andyc/auth/presentation/login/LoginViewModel.kt
@@ -3,13 +3,25 @@ package com.andyc.auth.presentation.login
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.andyc.auth.domain.AuthRepository
+import com.andyc.auth.domain.UserDataValidator
+import com.andyc.auth.presentation.R
+import com.andyc.core.domain.util.DataError
+import com.andyc.core.domain.util.Result
+import com.andyc.core.presentation.ui.UiText
+import com.andyc.core.presentation.ui.asUiText
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class LoginViewModel(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val userDataValidator: UserDataValidator
 ) : ViewModel() {
     var state by mutableStateOf(LoginState())
         private set
@@ -17,7 +29,51 @@ class LoginViewModel(
     private val eventChannel = Channel<LoginEvent>()
     val events = eventChannel.receiveAsFlow()
 
-    fun onAction(action: LoginAction) {
+    init {
+        combine(snapshotFlow { state.email.text }, snapshotFlow { state.password.text }) { email, password ->
+            state = state.copy(
+                canLogin = userDataValidator.isValidEmail(
+                    email = email.toString().trim()
+                ) && password.isNotEmpty()
+            )
+        }.launchIn(viewModelScope)
+    }
 
+    fun onAction(action: LoginAction) {
+        when (action) {
+            LoginAction.OnLoginClick -> login()
+            LoginAction.OnTogglePasswordVisibility -> {
+                state = state.copy(
+                    isPasswordVisible = !state.isPasswordVisible
+                )
+            }
+            else -> Unit
+        }
+    }
+
+    private fun login() {
+        viewModelScope.launch {
+            state = state.copy(isLoggingIn = true)
+            val result = authRepository.login(
+                email = state.email.text.toString().trim(),
+                password = state.password.text.toString()
+            )
+            state = state.copy(isLoggingIn = false)
+
+            when (result) {
+                is Result.Error -> {
+                    if (result.error == DataError.Network.UNAUTHORIZED) {
+                        eventChannel.send(LoginEvent.Error(
+                            UiText.StringResource(R.string.error_email_password_incorrect)
+                        ))
+                    } else {
+                        eventChannel.send(LoginEvent.Error(result.error.asUiText()))
+                    }
+                }
+                is Result.Success -> {
+                    eventChannel.send(LoginEvent.LoginSuccess)
+                }
+            }
+        }
     }
 }
