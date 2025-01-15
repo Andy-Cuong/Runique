530e783e2bf93ea7294c2dccaf7c3a9f7155ea3a
Andy Cuong <nguyenduccuong11102003@gmail.com>
2025-01-15 06:32:26 +0200

    Save pending-to-be-synced runs

--- core/database/src/main/java/com/andyc/core/database/RunDatabase.kt
+++ core/database/src/main/java/com/andyc/core/database/RunDatabase.kt
@@ -3,9 +3,19 @@ package com.andyc.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.andyc.core.database.dao.RunDao
+import com.andyc.core.database.dao.RunPendingSyncDao
+import com.andyc.core.database.entity.DeletedRunSyncEntity
 import com.andyc.core.database.entity.RunEntity
+import com.andyc.core.database.entity.RunPendingSyncEntity
 
-@Database(entities = [RunEntity::class], version = 1)
+@Database(
+    entities = [
+        RunEntity::class,
+        RunPendingSyncEntity::class,
+        DeletedRunSyncEntity::class
+               ],
+    version = 1)
 abstract class RunDatabase: RoomDatabase() {
     abstract val runDao: RunDao
+    abstract val runPendingSyncDao: RunPendingSyncDao
 }
