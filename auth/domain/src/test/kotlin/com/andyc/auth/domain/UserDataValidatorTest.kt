cfe284fb80ae26d9046f9711169871ab0fd4a9d2
Andy Cuong <nguyenduccuong11102003@gmail.com>
2025-01-23 08:02:59 +0200

    Set JUnit5 test dependencies

---
+++ auth/domain/src/test/kotlin/com/andyc/auth/domain/UserDataValidatorTest.kt
@@ -0,0 +1,14 @@
+package com.andyc.auth.domain
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class UserDataValidatorTest {
+
+    @Test
+    fun testAddingTwoNumbers() {
+        assertThat(2 + 2).isEqualTo(4)
+    }
+}
